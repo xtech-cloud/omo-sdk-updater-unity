6d91694f1b909b40609041900fc9f3b12bcb68c8
Easlee Liu <liu@easlee.me>
2018-08-22 22:22:39 +0800

    [FIXED] mcs.bat

--- mcs.bat
+++ mcs.bat
@@ -7,7 +7,7 @@ set depref=.\unitysln\Updater\Assets\OMO\UniLibrary\uniLitJson.dll
 ::----------------------------------
 MD %libpath%
 set outfile=%libpath%\%name%.dll
-set srcpath=.\unitysln\Updater\Assets\OMO\Updater
+set srcpath=.\unitysln\Updater\Assets\OMO\Updater\Scripts
 call "%UNITY_ROOT%\Editor\Data\Mono\bin\smcs" -target:library -r:"%UNITY_ROOT%\Editor\Data\Managed\UnityEngine.dll";"%UNITY_ROOT%\Editor\Data\UnityExtensions\Unity\GUISystem\UnityEngine.UI.dll" -out:%outfile% -recurse:%srcpath%\*.cs -reference:%depref%;%libref%
 echo FINISH
 pause
